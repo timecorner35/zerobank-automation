310a9af322cccce99b4b242ecc6425065844fcc8
timecorner35 <adamimdj@gmail.com>
2021-02-05 13:11:57 +0000

    Hooks addded screenshot

---
+++ src/test/java/com/zerobank/stepdefinitions/Hooks.java
@@ -0,0 +1,17 @@
+package com.zerobank.stepdefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    @After
+    public void close(Scenario scenario){
+        if (scenario.isFailed()){
+            final byte [] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
+}
