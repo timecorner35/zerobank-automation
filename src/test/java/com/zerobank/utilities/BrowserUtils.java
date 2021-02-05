719d03d3ee97db07b5a0eb76fe50a8b813b019bf
timecorner35 <adamimdj@gmail.com>
2021-02-05 18:02:56 +0000

    BrowserUtils updated

--- src/test/java/com/zerobank/utilities/BrowserUtils.java
+++ src/test/java/com/zerobank/utilities/BrowserUtils.java
@@ -1,4 +1,22 @@
 package com.zerobank.utilities;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
 public class BrowserUtils {
+    public static void clickWithJS(WebElement element) {
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].click();", element);
+    }
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+    public static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
