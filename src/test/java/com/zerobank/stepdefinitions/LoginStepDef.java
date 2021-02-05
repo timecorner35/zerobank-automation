55b6209f32995746d5ea12f699a916c2a9508187
timecorner35 <adamimdj@gmail.com>
2021-02-05 15:12:13 +0000

    LoginPage added

--- src/test/java/com/zerobank/stepdefinitions/LoginStepDef.java
+++ src/test/java/com/zerobank/stepdefinitions/LoginStepDef.java
@@ -1,15 +1,22 @@
 package com.zerobank.stepdefinitions;
 
+import com.zerobank.pages.LoginPage;
 import com.zerobank.utilities.ConfigurationReader;
 import com.zerobank.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDef {
 
     @Given("the user is logged in")
     public void the_user_is_logged_in() {
         Driver.get().get(ConfigurationReader.get("url"));
+        new LoginPage().login();
+        String actualTitle = Driver.get().getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+//        String expectedTitle = ""
+//        Assert.assertEquals();
 
     }
 
