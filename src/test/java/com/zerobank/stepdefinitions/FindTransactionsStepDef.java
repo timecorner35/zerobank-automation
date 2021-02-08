7b65e9de98630530301d3590aa4422012b5fc536
timecorner35 <adamimdj@gmail.com>
2021-02-08 09:04:13 +0000

    date compare completed

--- src/test/java/com/zerobank/stepdefinitions/FindTransactionsStepDef.java
+++ src/test/java/com/zerobank/stepdefinitions/FindTransactionsStepDef.java
@@ -11,7 +11,10 @@ import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 
 public class FindTransactionsStepDef {
@@ -19,24 +22,36 @@ public class FindTransactionsStepDef {
     public void the_user_accesses_the_Find_Transactions_tab() {
         new DashboardPage().accountActivity.click();
         AccountActivityPage accountActivityPage = new AccountActivityPage();
-       accountActivityPage.findTransactions.click();
+        accountActivityPage.findTransactions.click();
 
     }
+
     @When("the user enters date range from {string} to {string}")
     public void the_user_enters_date_range_from_to(String string, String string2) {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
-        accountActivityPage.dateFrom.sendKeys("2012-09-01");
-        accountActivityPage.dateTo.sendKeys("2012-09-06");
+        accountActivityPage.dateFrom.sendKeys(string);
+        accountActivityPage.dateTo.sendKeys(string2);
 
 
     }
+
     @Then("results table should only show transactions dates between {string} to {string}")
-    public void results_table_should_only_show_transactions_dates_between_to(String string, String string2) {
+    public void results_table_should_only_show_transactions_dates_between_to(String string, String string2) throws ParseException {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
-
-
-
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date fromDate = simpleDateFormat.parse(string);
+        Date toDate = simpleDateFormat.parse(string2);
+        for (WebElement webElement : accountActivityPage.dateColomn) {
+            Date date = simpleDateFormat.parse(webElement.getText());
+            System.out.println(date);
+            if (!(date.compareTo(fromDate)>=0&&date.compareTo(toDate)<=0)){
+                Assert.fail();
+            }else Assert.assertTrue(true);
+        }
     }
+
+
+
 
 
     @When("clicks search")
@@ -68,7 +83,7 @@ public class FindTransactionsStepDef {
     @Then("results table should only show descriptions containing {string}")
     public void results_table_should_only_show_descriptions_containing(String string) throws Throwable {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
-        if (accountActivityPage.tablerows.size()==0) {
+        if (accountActivityPage.tablerows.size() == 0) {
             Assert.assertTrue(false);
 
         }
@@ -90,7 +105,7 @@ public class FindTransactionsStepDef {
     public void results_table_should_show_at_least_one_result_under_Deposit() {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
         for (WebElement webElement : accountActivityPage.depositColomn) {
-            if(!webElement.getText().isBlank()){
+            if (!webElement.getText().isBlank()) {
                 Assert.assertTrue(true);
                 return;
             }
@@ -102,7 +117,7 @@ public class FindTransactionsStepDef {
     public void results_table_should_show_at_least_one_result_under_Withdrawal() {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
         for (WebElement webElement : accountActivityPage.withdrawColomn) {
-            if(!webElement.getText().isBlank()){
+            if (!webElement.getText().isBlank()) {
                 Assert.assertTrue(true);
                 return;
             }
@@ -112,7 +127,7 @@ public class FindTransactionsStepDef {
 
     @When("user selects type {string}")
     public void user_selects_type(String string) {
-        AccountActivityPage accountActivityPage =new AccountActivityPage();
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
         Select select = new Select(accountActivityPage.dropdown2);
         select.selectByValue(string.toUpperCase());
         accountActivityPage.searchButton.click();
@@ -124,7 +139,7 @@ public class FindTransactionsStepDef {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
         for (WebElement webElement : accountActivityPage.withdrawColomn) {
             System.out.println(webElement.getText());
-            if(!webElement.getText().isBlank()){
+            if (!webElement.getText().isBlank()) {
                 Assert.fail();
                 return;
             }
@@ -138,7 +153,7 @@ public class FindTransactionsStepDef {
         AccountActivityPage accountActivityPage = new AccountActivityPage();
         for (WebElement webElement : accountActivityPage.depositColomn) {
             System.out.println(webElement.getText());
-            if(!webElement.getText().isBlank()){
+            if (!webElement.getText().isBlank()) {
                 Assert.fail();
                 return;
             }
